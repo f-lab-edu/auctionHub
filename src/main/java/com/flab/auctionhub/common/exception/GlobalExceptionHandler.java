2d727f38cba853f03f51fc06ee0069bf1d665cc3
jeongmin.kim <systemdata227@gmail.com>
2023-09-21 01:15:58 +0900

    [refactor] 코드리뷰 반영

--- src/main/java/com/flab/auctionhub/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/flab/auctionhub/common/exception/GlobalExceptionHandler.java
@@ -10,12 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
-@RestControllerAdvice
+@Slf4j // 로깅을 쉽게 구현할 수 있도록 도와주는 롬복 어노테이션
+@RestControllerAdvice // @ControllerAdvice + @ResponseBody 모든 컨트롤러에 대한, 전역적으로 적용되는 예외 처리를 담당하는 클래스를 정의할 때 사용한다.
 public class GlobalExceptionHandler {
 
     private static String getCurrentTime() {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
     private ResponseEntity<ErrorResponse> buildAndReturnResponse(HttpStatus status, String message) {
