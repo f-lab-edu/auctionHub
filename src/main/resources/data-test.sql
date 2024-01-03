213f804122913134a92dbd2ba148a815e45d47cc
jammini <systemdata227@gmail.com>
2024-01-03 23:24:15 +0900

    [feature] 프로필별 설정 변경

---
+++ src/main/resources/data-test.sql
@@ -0,0 +1,8 @@
+INSERT INTO CATEGORY(name, is_deleted, created_at, created_by)
+VALUES ('남성의류', 'N', now(), 'A'),
+       ('여성의류', 'N', now(), 'A'),
+       ('가방', 'N', now(), 'A'),
+       ('신발', 'N', now(), 'A'),
+       ('생활용품', 'N', now(), 'A'),
+       ('건강', 'N', now(), 'A');
+
