afd49fa57418a392ae9ebfc59aed4b60e63e2ca2
jammini <systemdata227@gmail.com>
2024-02-04 23:41:04 +0900

    [feature] 세션 불일치 문제 해결

--- src/main/java/com/flab/auctionhub/AuctionHubApplication.java
+++ src/main/java/com/flab/auctionhub/AuctionHubApplication.java
@@ -2,6 +2,7 @@ package com.flab.auctionhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 public class AuctionHubApplication {
