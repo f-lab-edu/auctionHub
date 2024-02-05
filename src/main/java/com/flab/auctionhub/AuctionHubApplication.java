fc803fca0fdbe8227b40b2ca2f32a7cdc0669ecd
jammini <systemdata227@gmail.com>
2024-02-05 18:52:41 +0900

    [refactor] 사용하지 않은 import문 제거

--- src/main/java/com/flab/auctionhub/AuctionHubApplication.java
+++ src/main/java/com/flab/auctionhub/AuctionHubApplication.java
@@ -2,7 +2,6 @@ package com.flab.auctionhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 public class AuctionHubApplication {
