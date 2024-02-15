6173b359a7ea2094b33b5108ffc3a658a30add25
jammini <systemdata227@gmail.com>
2024-02-15 17:11:31 +0900

    [refactor] 패키지 변경

--- src/main/java/com/flab/auctionhub/common/config/RedisConfig.java
+++ src/main/java/com/flab/auctionhub/common/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.flab.auctionhub;
+package com.flab.auctionhub.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/flab/auctionhub/common/config/SecurityConfig.java
+++ src/main/java/com/flab/auctionhub/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.flab.auctionhub;
+package com.flab.auctionhub.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
